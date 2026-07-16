864e5a73d20d84d9e7cda67f42a5805b9480b0e6
prog202 <meteturan0706@gmail.com>
2026-07-16 18:41:18 +0300

    Smth happened

--- utils.py
+++ utils.py
@@ -9,11 +9,11 @@ def load_config():
             f.write("{}")
     with open(CONFIG,"r") as f:
         return dict(load(f))
-    
+
 def change_config(conf_obj):
     with open(CONFIG,"w") as f:
         dump(conf_obj, f)
-        
+
 def read_f(file):
     with open(file, "r") as f:
         return f.read()
@@ -21,7 +21,7 @@ def read_f(file):
 def write_f(file, content):
     with open(file, "w") as f:
         f.write(content)
-    
+
 def help():
     print("pickett add <KEY> <FILE>\npickett kill <KEY>/all")
     print("pickett truncate <KEY>/all\npickett clean <KEY>/all")
